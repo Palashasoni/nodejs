0b1c4949a5417c90d29464a62356302d04bb9cd7
Palasha Soni <palashasoni5@gmail.com>
2024-01-19 18:40:36 +0530

    Modified files

--- src/app.js
+++ src/app.js
@@ -1,2 +1,55 @@
-const { v4 : uuidv4} = require("uuid")
-console.log(uuidv4())
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const customers = [
+    {
+        name: "ABC",
+        industry:"music"
+    },
+    {
+        name: "DEF",
+        industry:"sports"
+    },
+    {
+        name:"GHI",
+        industry: "medicine"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send("Welcome")
+})
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers})
+})
+
+
+app.post('/', (req,res) => {
+    res.send("This is a post request")
+})
+
+app.post("/api/customers", (req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
+const start = async() =>{
+    try{
+        await mongoose.connect('mongodb+srv://palashasoni5:notpassword@cluster0.zg9lngy.mongodb.net/')
+        app.listen(PORT, ()=>{
+            console.log("App listening on port" + PORT)
+        })
+        }catch(e){
+            console.log(e.message)
+        }}
+
+
+start()
